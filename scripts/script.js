5223eb3fd1636ec225ef81bcbb6ea6bf505e7dc0
Tindyy <vdgraaf.tessa@gmail.com>
2023-09-17 20:07:01 +0200

    caroussel start F1

---
+++ scripts/script.js
@@ -0,0 +1,34 @@
+const carousel = document.querySelector(".carousel");
+const slides = document.querySelectorAll(".carousel-slide");
+const prevButton = document.querySelector(".prev-button");
+const nextButton = document.querySelector(".next-button");
+let currentIndex = 0;
+
+function updateCarousel() {
+  const translateXValue = -currentIndex * 100;
+  carousel.style.transform = `translateX(${translateXValue}%)`;
+
+  slides.forEach((slide, index) => {
+    if (index === currentIndex) {
+      slide.classList.add("active");
+    } else {
+      slide.classList.remove("active");
+    }
+  });
+}
+
+function nextSlide() {
+  currentIndex = (currentIndex + 1) % slides.length;
+  updateCarousel();
+}
+
+function prevSlide() {
+  currentIndex = (currentIndex - 1 + slides.length) % slides.length;
+  updateCarousel();
+}
+
+nextButton.addEventListener("click", nextSlide);
+prevButton.addEventListener("click", prevSlide);
+
+// Initial call to update the carousel
+updateCarousel();
