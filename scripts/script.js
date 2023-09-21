fa23d9d1ebc4fc36a0b7200c822a8583be038b8d
Latoya <76912011+latoyaln@users.noreply.github.com>
2023-09-21 23:18:25 +0200

    added pgs groep

--- scripts/script.js
+++ scripts/script.js
@@ -86,31 +86,6 @@ document.body.addEventListener("mouseleave", function () {
 // Initialize scroller
 scrollIt(0); // Set initial position to the left
 
-
-const card = document.querySelector(".card");
-
-card.addEventListener("mousemove", cardMouseMove);
-
-function cardMouseMove(event){
-const cardWidth = card.offsetWidth;
-const cardHeight = card.offsetHeight;
-const centerX = card.offsetLeft + cardWidth/2;
-const centerY = card.offsetTop + cardHeight/2;
-const mouseX = event.clientX - centerX;
-const mouseY = event.clientY - centerY;
-const rotateX = ((-1)*25*mouseY/(cardHeight/2)).toFixed(2);
-const rotateY = (25*mouseX/(cardWidth/2)).toFixed(2);
-
-card.style.transform = `perspective(1000px) rotateX(${rotateX}deg) rotateY(${rotateY}deg)`;
-}
-
-const expandButton = document.querySelector('header button')
-expandButton.addEventListener('click', expand)
-
-function expand () {
-  document.body.classList.toggle('expand')
-}
-
 function initializeCarousel(container) {
   const cid = container.id;
   const carousel = container.querySelector(".carousel");
