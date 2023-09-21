f36372a42c91fa5c1464c6fb1498b121ad666b29
Tindyy <vdgraaf.tessa@gmail.com>
2023-09-21 13:07:17 +0200

    Redid JS

--- scripts/script.js
+++ scripts/script.js
@@ -1,42 +1,43 @@
-const carouselContainers = document.querySelectorAll(".carousel-container");
+function initializeCarousel(container) {
+  const cid = container.id;
+  const carousel = container.querySelector(".carousel");
+  const slides = container.querySelectorAll(".carousel-slide");
+  const prevButton = container.querySelector(".prev-button");
+  const nextButton = container.querySelector(".next-button");
+  let currentIndex = 0;
 
-carouselContainers.forEach((container) =>{
-const carousel = document.querySelector(".carousel");
-const slides = document.querySelectorAll(".carousel-slide");
-const prevButton = document.querySelector(".prev-button");
-const nextButton = document.querySelector(".next-button");
-let currentIndex = 0;
+  nextButton.addEventListener("click", () => nextSlide(cid));
+  prevButton.addEventListener("click", () => prevSlide(cid));
 
-function updateCarousel() {
-  const translateXValue = -currentIndex * 100;
-  carousel.style.transform = `translateX(${translateXValue}%)`;
+  function updateCarousel() {
+    const translateXValue = -currentIndex * 100;
+    carousel.style.transform = `translateX(${translateXValue}%)`;
 
-  slides.forEach((slide, index) => {
-    if (index === currentIndex) {
-      slide.classList.add("active");
-    } else {
-      slide.classList.remove("active");
-    }
-  });
-}
+    slides.forEach((slide, index) => {
+      if (index === currentIndex) {
+        slide.classList.add("active");
+      } else {
+        slide.classList.remove("active");
+      }
+    });
+  }
 
-function nextSlide() {
-  currentIndex = (currentIndex + 1) % slides.length;
+  function nextSlide() {
+    currentIndex = (currentIndex + 1) % slides.length;
+    updateCarousel();
+  }
+
+  function prevSlide() {
+    currentIndex = (currentIndex - 1 + slides.length) % slides.length;
+    updateCarousel();
+  }
+
+  // Initial call to update the carousel
   updateCarousel();
 }
 
-function prevSlide() {
-  currentIndex = (currentIndex - 1 + slides.length) % slides.length;
-  updateCarousel();
-}
+const containers = document.querySelectorAll(".container");
 
-nextButton.addEventListener("click", nextSlide);
-prevButton.addEventListener("click", prevSlide);
-
-// Initial call to update the carousel
-updateCarousel();
+containers.forEach((container) => {
+  initializeCarousel(container);
 });
-
-
-
-
